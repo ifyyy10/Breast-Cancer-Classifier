58f73c1d556028a4857bcbc2069390400a49074d
ifyyy10 <75942656+ifyyy10@users.noreply.github.com>
2024-09-08 00:48:01 +0100

    Update breast_cancer_classifier.py

--- breast_cancer_classifier.py
+++ breast_cancer_classifier.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from tensorflow.keras.models import load_model
+#from tensorflow.keras.models import load_model
 from PIL import Image
 import gdown
 import os
