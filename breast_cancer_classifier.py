c40e5784af1171dddce38938af541714080c4292
ifyyy10 <75942656+ifyyy10@users.noreply.github.com>
2024-09-07 23:50:06 +0100

    Update breast_cancer_classifier.py

--- breast_cancer_classifier.py
+++ breast_cancer_classifier.py
@@ -11,7 +11,6 @@ IMPORT ALL LIBRARIES AND MOUNT GOOGLE DRIVE
 
 # import all libraries
 import os
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras import layers, models, Input
 import matplotlib.pyplot as plt
